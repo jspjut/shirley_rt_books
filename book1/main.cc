420b8c8005b733a87411e418fde2277e673596ff
Josef Spjut <josef.spjut@gmail.com>
2016-06-07 15:09:55 -0400

    Switch to using stb_image_write to write png instead of ppm.

--- book1/main.cc
+++ book1/main.cc
@@ -1,10 +1,16 @@
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 int main()
 {
     int nx = 200;
     int ny = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    // output buffer
+    unsigned char *data = new unsigned char[nx*ny*3];
+    // set image to gradient
     for (int j = ny-1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
@@ -15,7 +21,12 @@ int main()
             int ir = int(255.99*r);
             int ig = int(255.99*g);
             int ib = int(255.99*b);
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            data[(i+j*nx)*3+0] = ir;
+            data[(i+j*nx)*3+1] = ig;
+            data[(i+j*nx)*3+2] = ib;
         }
     }
+    // write buffer to file
+    stbi_write_png("out.png", nx, ny, 3, data, 0);
+    delete [] data;
 }

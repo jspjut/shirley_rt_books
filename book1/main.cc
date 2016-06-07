550ac8618bd9df5a9b4804b4b6798e27cdcef38a
Josef Spjut <josef.spjut@gmail.com>
2016-06-07 16:24:37 -0400

    Chapter 4 complete. Intersect a single sphere.

--- book1/main.cc
+++ book1/main.cc
@@ -9,9 +9,22 @@
 // include ray and vec3 classes
 #include "ray.h"
 
-// linear blend of white and blue depending on y coordinate
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+// test whether a sphere is hit
 vec3 color(const ray& r) 
 {
+    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1, 0, 0);
+    // fallback to the blue/white gradient
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);

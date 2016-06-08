53820efe36a9875e517dd2afd6855e80bb7d1096
Josef Spjut <josef.spjut@gmail.com>
2016-06-08 11:16:30 -0400

    Moved camera to separate header, and added sampling. Finished chapter 6.

--- book1/main.cc
+++ book1/main.cc
@@ -9,6 +9,7 @@
 // include my classes
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 
 // color and fallback to background color
 vec3 color(const ray& r, hitable *world) 
@@ -31,14 +32,10 @@ int main()
 {
     int nx = 200;
     int ny = 100;
+    int ns = 100; // sample count
+
     // output buffer
     unsigned char *data = new unsigned char[nx*ny*3];
-
-    // camera setup from chapter 3
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
 
     // object list
     hitable *list[2];
@@ -46,24 +43,36 @@ int main()
     list[1] = new sphere(vec3(0,-100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
 
+    // camera
+    camera cam;
+
     // loop over pixels
     for (int j = ny-1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col(0,0,0);
 
-            // vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            for (int s=0; s < ns; s++)
+            {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
+            // normalize color
+            col /= float(ns);
 
             // store color in image buffer
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
+
             // this fixed a bug in the chapter2 y mapping for the image
             int pix_id = (i+nx*(ny-1)-j*nx)*3;
+
             data[pix_id+0] = ir;
             data[pix_id+1] = ig;
             data[pix_id+2] = ib;

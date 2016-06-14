1c080b98b7c8c536badc025a0efcf00952a8ce69
Josef Spjut <josef.spjut@gmail.com>
2016-06-14 15:20:12 -0400

    material.h added to separate materials into their own file.

--- book1/main.cc
+++ book1/main.cc
@@ -10,133 +10,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
-
-// choose a random vector in the unit sphere
-vec3 random_in_unit_sphere()
-{
-    vec3 p;
-    do {
-        p = 2.0*vec3(drand48(),drand48(),drand48()) - vec3(1,1,1);
-    } while (p.squared_length() >= 1.0);
-    return p;
-}
-
-// abstract class
-class material
-{
-    public:
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
-};
-
-class lambertian : public material {
-    public:
-        lambertian(const vec3& a) : albedo(a) {}
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
-        {
-            vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-            scattered = ray(rec.p, target - rec.p);
-            attenuation = albedo;
-            return true;
-        }
-
-        vec3 albedo;
-};
-
-vec3 reflect(const vec3& v, const vec3& n)
-{
-    return v - 2*dot(v,n)*n;
-}
-
-class metal : public material {
-    public:
-        metal(const vec3& a, float f) : albedo(a) { if (f<1) fuzz = f; else fuzz = 1; }
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
-        {
-            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
-            attenuation = albedo;
-            return (dot(scattered.direction(), rec.normal) > 0);
-        }
-
-        vec3 albedo;
-        float fuzz;
-};
-
-// returns true if refraction happens. Also returns the refraction direction.
-bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
-{
-    vec3 uv = unit_vector(v);
-    float dt = dot(uv, n);
-    float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
-    if (discriminant > 0)
-    {
-        refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// approximation of steep angles by Christophe Schlick
-float schlick(float cosine, float ref_idx)
-{
-    float r0 = (1-ref_idx) / (1+ref_idx);
-    r0 = r0*r0;
-    return r0 + (1-r0)*pow((1-cosine),5);
-}
-
-class dielectric : public material
-{
-    public:
-        dielectric(float ri) : ref_idx(ri) {}
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
-        {
-            vec3 outward_normal;
-            vec3 reflected = reflect(r_in.direction(), rec.normal);
-            float ni_over_nt;
-            attenuation = vec3(1.0, 1.0, 1.0);
-            vec3 refracted;
-            float reflect_prob;
-            float cosine;
-            // check which side of normal the ray is
-            if (dot(r_in.direction(), rec.normal) > 0)
-            {
-                outward_normal = -rec.normal;
-                ni_over_nt = ref_idx;
-                cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
-            }
-            else
-            {
-                outward_normal = rec.normal;
-                ni_over_nt = 1.0 / ref_idx;
-                cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
-            }
-            // check if refraction happens
-            if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
-            {
-                reflect_prob = schlick(cosine, ref_idx);
-            }
-            else
-            {
-                // always reflect
-                reflect_prob = 1.0;
-            }
-            //choose either reflect or refract based on probability
-            if (drand48() < reflect_prob)
-            {
-                scattered = ray(rec.p, reflected);
-            }
-            else
-            {
-                scattered = ray(rec.p, refracted);
-            }
-            return true;
-        }
-
-        float ref_idx;
-};
+#include "material.h"
 
 // color and fallback to background color
 vec3 color(const ray& r, hitable *world, int depth) 
@@ -166,7 +40,7 @@ vec3 color(const ray& r, hitable *world, int depth)
     }
     else
     {
-        // fallback to the blue/white gradient
+        // fallback to the blue/white gradient (sky)
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
         return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
@@ -181,7 +55,7 @@ int main()
     // int ny = 1000;
     // sample count: 10 is very fast and noisy, 100 is reasonable (used in book)
     // 1000 is kinda slow but looks pretty good, more is probably needed for quality
-    int ns = 1000;
+    int ns = 100;
 
     // output buffer
     unsigned char *data = new unsigned char[nx*ny*3];
@@ -194,13 +68,13 @@ int main()
     // hitable *world = new hitable_list(list, 2);
 
     // object list from chapter 9
-    hitable *list[5];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-    list[1] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
-    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
-    hitable *world = new hitable_list(list, 5);
+    // hitable *list[5];
+    // list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+    // list[1] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+    // list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
+    // list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    // list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    // hitable *world = new hitable_list(list, 5);
 
     // object list from Chapter 8.
     // hitable *list[4];
@@ -211,23 +85,23 @@ int main()
     // hitable *world = new hitable_list(list, 4);
 
     // Objects made with Audrey!
-    // hitable *list[6];
-    // list[0] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.545,0.27,0.075)));//139, 69, 19
-    // // upper row blue, black, red
-    // list[1] = new sphere(vec3(-0.55,0.0,-1), 0.25, new metal(vec3(0.0,0.4,0.8), 0.9));
-    // list[2] = new sphere(vec3(0,0.0,-1), 0.25, new metal(vec3(0.116,0.1,0.1), 0.9));
-    // list[3] = new sphere(vec3(0.55,0.0,-1), 0.25, new metal(vec3(0.875,0.208,0.29), 0.9));
-    // // lower row yellow, green
-    // list[4] = new sphere(vec3(-0.275,-0.25,-1), 0.25, new metal(vec3(0.953,0.714,0.302), 0.7));
-    // list[5] = new sphere(vec3(0.275,-0.25,-1), 0.25, new metal(vec3(0.114,0.613,0.333), 0.7));
-    // hitable *world = new hitable_list(list, 6);
+    hitable *list[6];
+    list[0] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.545,0.27,0.075)));//139, 69, 19
+    // upper row blue, black, red
+    list[1] = new sphere(vec3(-0.55,0.0,-1), 0.25, new metal(vec3(0.0,0.4,0.8), 0.9));
+    list[2] = new sphere(vec3(0,0.0,-1), 0.25, new metal(vec3(0.116,0.1,0.1), 0.9));
+    list[3] = new sphere(vec3(0.55,0.0,-1), 0.25, new metal(vec3(0.875,0.208,0.29), 0.9));
+    // lower row yellow, green
+    list[4] = new sphere(vec3(-0.275,-0.25,-1), 0.25, new metal(vec3(0.953,0.714,0.302), 0.7));
+    list[5] = new sphere(vec3(0.275,-0.25,-1), 0.25, new metal(vec3(0.114,0.613,0.333), 0.7));
+    hitable *world = new hitable_list(list, 6);
 
     // camera
     vec3 lookfrom(3,3,2);
     vec3 lookat(0,0,-1);
     float dist_to_focus = (lookfrom-lookat).length();
     float aperture = 2.0;
-    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vec3(0,1,0), 15, float(nx)/float(ny), aperture, dist_to_focus);
 
     // loop over pixels
     // first hit for debugging

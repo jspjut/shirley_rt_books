b931d5203a34021524d8e9cddfefb124184bce17
Josef Spjut <josef.spjut@gmail.com>
2016-06-14 11:54:22 -0400

    First update to camera based on chapter 10.

--- book1/main.cc
+++ book1/main.cc
@@ -187,13 +187,20 @@ int main()
     unsigned char *data = new unsigned char[nx*ny*3];
 
     // object list
-    hitable *list[5];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-    list[1] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
-    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
-    hitable *world = new hitable_list(list, 5);
+    hitable *list[2];
+    float R = cos(M_PI/4);
+    list[0] = new sphere(vec3(-R,0,-1), R, new lambertian(vec3(0,0,1)));
+    list[1] = new sphere(vec3( R,0,-1), R, new lambertian(vec3(1,0,0.0)));
+    hitable *world = new hitable_list(list, 2);
+
+    // object list from chapter 9
+    // hitable *list[5];
+    // list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+    // list[1] = new sphere(vec3(0,-100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+    // list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
+    // list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    // list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    // hitable *world = new hitable_list(list, 5);
 
     // object list from Chapter 8.
     // hitable *list[4];
@@ -216,7 +223,7 @@ int main()
     // hitable *world = new hitable_list(list, 6);
 
     // camera
-    camera cam;
+    camera cam(90, float(nx)/float(ny));
 
     // loop over pixels
     // first hit for debugging
